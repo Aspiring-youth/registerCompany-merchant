416512b919286f8a22ba3316c94a6bd0d58d4b9b
jinlongchao <jlc123456>
2022-02-14 09:43:20 +0800

    提交人： jinlongchao 提交时间： 2022-02-14 提交内容： jar包优化

--- merthant-api/src/main/java/org/merchant/api/MerchantApplication.java
+++ merthant-api/src/main/java/org/merchant/api/MerchantApplication.java
@@ -1,13 +1,15 @@
 package org.merchant.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@SpringBootApplication(scanBasePackages = {"org.merchant.db", "org.merchant.api"})
-//@MapperScan("org.merchant.db.dao")
+@MapperScan("org.merchant.db.dao")
 //@EnableTransactionManagement
-@EnableScheduling
+@SpringBootApplication
+//@EnableScheduling
 public class MerchantApplication {
     public static void main(String[] args) {
         SpringApplication.run(MerchantApplication.class, args);
